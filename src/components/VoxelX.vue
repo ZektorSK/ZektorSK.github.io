acaf9287bc293e69bd8b22cd3dfa08d97242ccf9
zektor <dankomajchrak423@gmail.com>
2022-03-17 23:57:48 +0100

    first few designs of the voxel display

--- src/components/VoxelX.vue
+++ src/components/VoxelX.vue
@@ -1,15 +1,14 @@
 <template>
-    <div class="flex justify-center justify-items-center">
-        <div id="image w-80 h-80">
-            <img :src="voxel.source">
+    <div class="flex justify-center justify-items-center h-screen grow-0 shrink-0">
+        <div id="image" class="mr-[100px] pb-[60px] flex justify-center justify-items-center items-center h-screen">
+            <img :src="voxel.source" class="w-[650px] h-[650px] rounded-full">
         </div>
-        <div id="text-container">
-            <div id="description">
-                <img id="arrow-left">
-                <h1>{{voxel.name}}</h1>
-                <img id="arrow-right">
-                <p>{{voxel.description}}</p>
-            </div>
+        <div id="description" class="h-[300px] w-[700px] flex justify-center justify-items-center items-center">
+            <img id="arrow-left" class="w-8 h-8 reverse-arrow" src="../assets/arrow.png">
+            <h1 class="font-serif text-7xl font-bold text-center">{{voxel.name}}</h1>
+            <img id="arrow-right" class="w-8 h-8" src="../assets/arrow.png">
+            <hr>
+            <p>{{voxel.description}}</p>
         </div>
     </div>
 </template>
@@ -36,3 +35,9 @@ export default {
 
 }
 </script>
+
+<style scoped>
+.reverse-arrow{
+    transform: scaleX(-1);
+}
+</style>
