e93385223d1733b50645c0d30fee2e31147a561c
zektor <dankomajchrak423@gmail.com>
2022-03-19 00:06:45 +0100

    Fixed design issues, added move functionality

--- src/App.vue
+++ src/App.vue
@@ -23,16 +23,45 @@ export default {
         {
             id:0,
             source:"/voxels/0.png",
-            name:"Barcelona",
-            description:"My first ever city done in the CryptoCities project. The capital of Spain with it's majestic cathedral and sophisticated street layout."
+            name:"Chernobyl",
+            description:"A depiction of how the nuclear catastrophy city looks like today."
         },
         {
             id:1,
             source:"/voxels/1.png",
-            name:"Beijing",
-            description:"No description yet."
+            name:"Harran",
+            description:"Fictional city in the middle-east that has been infected by a zombie virus."
+        },
+        {
+            id:2,
+            source:"/voxels/2.png",
+            name:"Stalingrad",
+            description:"A depiction of the city during the battle of Stalingrad."
+        },
+        {
+            id:3,
+            source:"/voxels/3.png",
+            name:"Tabriz",
+            description:"A major Iranian city, almost entirely encircled by mountains."
+        },
+        {
+            id:4,
+            source:"/voxels/4.png",
+            name:"Gaza City",
+            description:"A depiction of the Gaza strip capital soaked in conflict."
+        },
+        {
+            id:5,
+            source:"/voxels/5.png",
+            name:"Himeji Castle",
+            description:"The magnificent japanese feudal castle."
+        },
+        {
+            id:6,
+            source:"/voxels/6.png",
+            name:"Kuressaare",
+            description:"A peaceful little town on the island of Saaremaa in Estonia."
         }
-
       ]
     }
   },
--- src/components/VoxelX.vue
+++ src/components/VoxelX.vue
@@ -3,12 +3,13 @@
         <div id="image" class="mr-[100px] pb-[60px] flex justify-center justify-items-center items-center h-screen">
             <img :src="voxel.source" class="w-[650px] h-[650px] rounded-full">
         </div>
-        <div id="description" class="h-[300px] w-[700px] flex justify-center justify-items-center items-center">
-            <img id="arrow-left" class="w-8 h-8 reverse-arrow" src="../assets/arrow.png">
-            <h1 class="font-serif text-7xl font-bold text-center">{{voxel.name}}</h1>
-            <img id="arrow-right" class="w-8 h-8" src="../assets/arrow.png">
-            <hr>
-            <p>{{voxel.description}}</p>
+        <div id="description" class="h-[300px] w-[700px] mt-[250px] p-3 flex justify-center justify-items-center items-center shadow-md flex-col">
+            <div id="head" class="flex justify-center justify-items-center items-center flex-row underline decoration-4 underline-offset-8">
+                <img id="arrow-left" class="w-8 h-8 reverse-arrow m-5 cursor-pointer" src="../assets/arrow.png" @click="goLeft(voxel.id)">
+                <h1 class="font-serif text-7xl font-bold text-center">{{voxel.name}}</h1>
+                <img id="arrow-right" class="w-8 h-8 m-5 cursor-pointer" src="../assets/arrow.png" @click="goRight(voxel.id)">
+            </div>
+            <p class="text-center mt-6 text-lg font-mono font-medium">{{voxel.description}}</p>
         </div>
     </div>
 </template>
@@ -31,6 +32,27 @@ export default {
 
             return randObject;
         },
+        goLeft(i_id){
+            if(i_id < this.voxelsList.length && i_id > 0){
+                i_id -= 1;
+                this.voxel = this.voxelsList[i_id];
+                console.log("I moved left!");
+            }else{
+                i_id = (this.voxelsList.length) - 1;
+                this.voxel = this.voxelsList[i_id];
+                console.log("I went to the end!");
+            }
+        },
+        goRight(i_id){
+            if(i_id < this.voxelsList.length && i_id > 0){
+                i_id += 1;
+                this.voxel = this.voxelsList[i_id];
+                console.log("I moved right!");
+            }else{
+                this.voxel = this.voxelsList[0];
+                console.log("I went to the beginning!");
+            }
+        }
     }
 
 }
