6a3d19a232301ff570bc17541fc6eb6c8eb4f043
zektor <dankomajchrak423@gmail.com>
2022-03-27 23:38:31 +0200

    Finished

--- src/components/VoxelX.vue
+++ src/components/VoxelX.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="flex justify-center justify-items-center h-screen grow-0 shrink-0 text-white">
 
-        <div id="image" class="mr-[100px] pb-[60px] flex justify-center justify-items-center items-center h-screen">
+        <div id="image" class="mr-[80px] pb-[60px] flex justify-center justify-items-center items-center h-screen">
+            <img src="/arrow.png" class="reverse-arrow h-[70px] w-[100px] cursor-pointer" @click="removeCircle(this.voxel.id); goLeft(this.voxel.id); changeBG(this.voxel); changeCircle(this.voxel.id)">
             <img :src="voxel.source" class="w-[650px] h-[650px] border-4 border-white">
+            <img src="/arrow.png" class="h-[70px] w-[100px] cursor-pointer" @click="removeCircle(this.voxel.id); goRight(this.voxel.id); changeBG(this.voxel); changeCircle(this.voxel.id)">
         </div>
 
         <div id="description" class="h-[500px] w-[700px] mt-[200px] p-3 flex justify-center justify-items-center items-center shadow-md flex-col">
 
             <ul class="flex justify-center flex-row gap-2 mb-6">
                 <li v-for="(voxl, index) in voxelsList" :key="voxl">
-                    <img src="../assets/circle_empty.png" class="w-[20px] h-[20px] inverted" :id="index">
+                    <img src="/circle_empty.png" class="w-[20px] h-[20px] cursor-pointer" :id="index" @click="removeCircle(this.voxel.id); changePosition(voxl); changeCircle(voxl.id); changeBG(voxl)">
                 </li>
             </ul>
 
@@ -38,7 +40,6 @@ export default {
             const randObject = this.voxelsList[randIndex];
 
             this.changeBG(randObject);
-            this.changeCircle(randObject.id);
             return randObject;
         },
         goLeft(i_id){
@@ -54,7 +55,7 @@ export default {
             }
         },
         goRight(i_id){
-            if(i_id < this.voxelsList.length && i_id > 0){
+            if(i_id < this.voxelsList.length-1 && i_id >= 0){
                 i_id += 1;
                 this.voxel = this.voxelsList[i_id];
                 console.log("I moved right!");
@@ -68,19 +69,24 @@ export default {
             document.querySelector('body').setAttribute('style', `background-image: url(${CSS.escape(url_toblur)});`);
         },
         changeCircle(id){
-            document.querySelector('#'+id).attr('src', '../assets/circle_full.png');
+            document.getElementById(id).setAttribute('src', '/circle_full.png');
+        },
+        removeCircle(id){
+            document.getElementById(id).setAttribute('src', '/circle_empty.png');
+        },
+        changePosition(voxl){
+            this.voxel = voxl;
         }
-    }
+    },
 
+    mounted(){
+        this.changeCircle(this.voxel.id);
+    }
 }
 </script>
 
 <style scoped>
 .reverse-arrow{
     transform: scaleX(-1);
-}
-
-.inverted{
-    filter: invert(100%);
 }
 </style>
